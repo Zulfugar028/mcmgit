f2652ee7179320598105cf39571a12aa7c83a8d5
Zulfugar028 <gfotograf77@gmail.com>
2025-12-21 23:20:55 +0400

    Changes

--- app.js
+++ app.js
@@ -162,3 +162,18 @@ if (blogId) {
         })
         .catch(err => console.error("Detallar yüklənmədi:", err));
 }
+
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey && e.shiftKey && e.key === "I") ||
+    (e.ctrlKey && e.key === "U")
+  ) {
+    e.preventDefault();
+  }
+});
